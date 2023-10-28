6d3720805d48fb55e5f1f29be5beca9b93789240
Ricardo Garcia <luiz.rpgarcia@gmail.com>
2023-10-27 23:35:46 -0300

    finalizado

--- pollProject/urls.py
+++ pollProject/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('pollApp.urls')),
+    path('', include('pages.urls')),
+    path('polls/', include('pollApp.urls')),
 ]
